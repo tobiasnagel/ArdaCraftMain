9b97409f58eb1f091e3fdee92128bf2671fedc18
ArdaCraft <tona0429@gmail.com>
2015-01-30 12:59:19 +0100

    /gut command added

---
+++ src/me/tobi/acmain/commands/CmdGut.java
@@ -0,0 +1,31 @@
+package me.tobi.acmain.commands;
+
+import me.tobi.acmain.ArdaCraft;
+import me.tobi.acmain.message.Message;
+import me.tobi.acmain.rasse.Rasse;
+import me.tobi.acmain.rasse.Rasse.Attitude;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CmdGut implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		sender.sendMessage("§aGute Rassen:");
+		int count = 0;
+		for(Rasse r : Rasse.values()) {			
+			if(r.getAttitude() == Attitude.GOOD) {
+				Message m = new Message("§a#" + String.valueOf(count+1) + " §6" + r.getName());
+				m.setHoverText(r.getAbility());
+				Message[] msg = {m};
+				ArdaCraft.getCraftLogger().chatJSON(msg, (Player)sender);
+				count++;
+			}
+		}
+		return true;
+	}
+
+}
