9899fc4e07849d87455280dd9e04260a0fbbcc21
ArdaCraft <tona0429@gmail.com>
2015-01-30 13:07:49 +0100

    Constants for messages added

---
+++ src/me/tobi/acmain/message/Msg.java
@@ -0,0 +1,19 @@
+package me.tobi.acmain.message;
+
+public class Msg {
+	
+	public static final String PLUGIN_ENABLED = "§eArdaCraft Hauptplugin §aaktiviert§e!";
+	public static final String EVENT_JOIN_WELCOME = "§a§lHerzlich Willkommen auf §e§lArdaCraft";
+	public static final String COMMAND_PLAYER_ONLY = "Dieser Befehl ist nur für Spieler!";
+	public static final String COMMAND_NO_PERMISSION = "Du hast keine Berechtigung für diesen Befehl";
+	public static final String COMMAND_REGISTER_USAGE = "Benutzung: /register <Player> <Rasse> <Charakter>";
+	public static final String COMMAND_CHECKINAKTIVE_USAGE = "Benutzung: /checkinaktive";
+	public static final String COMMAND_REGISTER_TAKEN = "Der Charakter ist bereits vergeben!";
+	public static final String COMMAND_REQUEST_USAGE = "Benutzung: /request <Rasse> <Charaktername>";
+	public static final String COMMAND_REQUEST_INVALIDNAME = "Bitte wähle einen Herr der Ringe namen wie z.B. \"Gandalf\"!";
+	public static final String COMMAND_REQUEST_INVALIDRACE = "Bitte wähle eine Herr der Ringe Rasse wie z.B. \"Magier\"";
+	public static final String COMMAND_REQUEST_CHARAKTERTAKEN = "Dieser Charakter ist leider schon vergeben! Bitte wähle einen anderen!";
+	public static final String COMMAND_ADDWARP_USAGE = "Benutzung: /addwarp <inbuild_[ID]> <Stadtname>";
+	public static final String COMMAND_PLAYER_USAGE = "Benutzung: /player <Player>";
+	
+}
