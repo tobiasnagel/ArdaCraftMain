c268f115b89874c5c104126417364b4a150037c4
ArdaCraft <tona0429@gmail.com>
2015-01-30 12:57:40 +0100

    /list command added

---
+++ src/me/tobi/acmain/commands/CmdList.java
@@ -0,0 +1,26 @@
+package me.tobi.acmain.commands;
+
+import me.tobi.acmain.ArdaCraft;
+import me.tobi.acmain.rasse.Rasse;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CmdList implements CommandExecutor {
+
+	@SuppressWarnings({ "deprecation", "unused" })
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		for(Rasse r : Rasse.values()) {
+			String online = "";
+			online += r.getName();
+		}
+		for(Player p : ArdaCraft.getACServer().getOnlinePlayers()) {
+			
+		}
+		return true;
+	}
+
+}
