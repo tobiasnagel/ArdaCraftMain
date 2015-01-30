4b384bf8efdb467996875534ee160bcfed7a141d
ArdaCraft <tona0429@gmail.com>
2015-01-30 13:07:31 +0100

    Constants for permissions added

---
+++ src/me/tobi/acmain/message/Permission.java
@@ -0,0 +1,8 @@
+package me.tobi.acmain.message;
+
+public class Permission {
+	
+	public static final String CHECKINAKTIVE = "ardacraft.team.checkinaktive";
+	public static final String SPAWN = "ardacraft.team.spawn";
+	
+}
