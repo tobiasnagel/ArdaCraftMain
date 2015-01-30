563d20e72efa457eda8b025a1326ec80b2420621
ArdaCraft <tona0429@gmail.com>
2015-01-30 12:59:53 +0100

    /cmute command added

---
+++ src/me/tobi/acmain/commands/CmdCmute.java
@@ -0,0 +1,34 @@
+package me.tobi.acmain.commands;
+
+import me.tobi.acmain.ArdaCraft;
+import me.tobi.acmain.events.EvtHandler;
+import me.tobi.acmain.message.CraftLogger.Level;
+import me.tobi.acmain.message.Msg;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CmdCmute implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(sender != null) {
+			Player p = (Player)sender;
+			if(p.isOp()) {
+				if (EvtHandler.chatmuteActive == true) {
+					EvtHandler.chatmuteActive = false;
+				} else {
+					EvtHandler.chatmuteActive = true;
+				}
+			}else { //No permission
+				ArdaCraft.getCraftLogger().logToChat(Level.WARN, Msg.COMMAND_NO_PERMISSION, p);
+			}
+		}else {
+			ArdaCraft.getCraftLogger().logToConsole(Level.WARN, Msg.COMMAND_PLAYER_ONLY);
+		}
+		return true;
+	}
+
+}
